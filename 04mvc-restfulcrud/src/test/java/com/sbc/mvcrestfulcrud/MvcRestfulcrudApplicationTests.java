2fd5bc23e88faf295f3fc2caa1bd0203571b5ba4
Anonymous <13286985471@163.com>
2019-03-29 13:35:55 +0800

    1

---
+++ 04mvc-restfulcrud/src/test/java/com/sbc/mvcrestfulcrud/MvcRestfulcrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sbc.mvcrestfulcrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvcRestfulcrudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
