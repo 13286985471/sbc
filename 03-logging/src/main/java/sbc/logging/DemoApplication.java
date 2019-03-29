30dd18c7fcb15d0d1ca1e5d0b9ea87a2b9b1aaab
Anonymous <13286985471@163.com>
2019-03-29 13:17:02 +0800

    1

--- 03-logging/src/main/java/sbc/logging/DemoApplication.java
+++ 03-logging/src/main/java/sbc/logging/DemoApplication.java
@@ -2,8 +2,10 @@ package sbc.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@ResponseBody
 public class DemoApplication {
 
     public static void main(String[] args) {
--- 03-logging/src/test/java/sbc/logging/DemoApplicationTests.java
+++ 03-logging/src/test/java/sbc/logging/DemoApplicationTests.java
@@ -2,15 +2,24 @@ package sbc.logging;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+    Logger logger=LoggerFactory.getLogger(getClass());
     @Test
     public void contextLoads() {
+        logger.trace("trace");
+        logger.debug("这里是debug日志");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
     }
-
 }
