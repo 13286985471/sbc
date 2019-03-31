9aaf5f888f9043e7ee8259893a5d654d366a799d
Anonymous <13286985471@163.com>
2019-03-31 19:34:19 +0800

    1

--- 04mvc-restfulcrud/src/main/java/com/sbc/config/MyConfig.java
+++ 04mvc-restfulcrud/src/main/java/com/sbc/config/MyConfig.java
@@ -1,7 +1,10 @@
 package com.sbc.config;
 
+import com.sbc.component.MyLocaleResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +13,10 @@ public class MyConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
